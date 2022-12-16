bd6bc11847eb5c56af7eacff94765220457994ac
Flauz <dread94@gmail.com>
2022-12-16 11:22:41 +0100

    Dockerize Express App

--- back-end/index.js
+++ back-end/index.js
@@ -8,20 +8,10 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-async function main() {
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
-}
-
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
+// async function main() {
+//   const allUsers = await prisma.user.findMany()
+//   console.log(allUsers)
+// }
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
