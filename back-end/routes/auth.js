2e395e752c3a2b9a673d89d06399fee6a67714ee
Maxime MARCHAND <mmarchand6@myges.fr>
2022-12-29 00:40:43 +0100

    Mailer implementation

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,115 +1,116 @@
-const {Router} = require('express') ;
-const bcrypt = require('bcrypt') ;
-const jwt = require('jsonwebtoken') ;
-const {PrismaClient} = require('@prisma/client') ;
-// const sendEmail = require('../services/nodemailer');
-const prisma = new PrismaClient() ;
+const { Router } = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { PrismaClient } = require('@prisma/client');
+const sendEmail = require('../services/nodemailer');
 
-
-const router = Router() ;
+const prisma = new PrismaClient();
+const router = Router();
 
 router.post('/login', async (req, res) => {
-  const {email, password} = req.body ;
-  const user = await prisma.user.findUnique({where: {email}}) ;
-  if (user) {
-    const valid = await bcrypt.compare(password, user.password) ;
-    if (valid) {
-      const token = jwt.sign({id: user.id}, process.env.JWT_SECRET) ;
-      res.json({token}) ;
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (user) {
+        const valid = await bcrypt.compare(password, user.password);
+        if (valid) {
+            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+            res.status(200).json({ token });
+        } else {
+            res.status(401).json({ error: 'Invalid password' });
+        }
     } else {
-      res.status(401).json({error: 'Invalid password'}) ;
+        res.status(401).json({ error: 'Invalid email' });
     }
-  } else {
-    res.status(401).json({error: 'Invalid email'}) ;
-  }
 });
 
 router.post('/register', async (req, res) => {
-    const {email, password, firstName, lastName} = req.body ;
-    const user = await prisma.user.findUnique({where: {email}}) ;
+    const { email, password, firstName, lastName } = req.body;
+    const user = await prisma.user.findUnique({ where: { email } });
     if (user) {
-        res.status(409).json({error: 'Email already exists'}) ;
+        res.status(409).json({ error: 'Email already exists' });
     } else {
-        const hash = await bcrypt.hash(password, 10) ;
+        const hash = await bcrypt.hash(password, 10);
         const newUser = await prisma.user.create({
             data: {
                 email,
-                password: hash, 
-                firstName: firstName, 
-                lastName: lastName, 
+                password: hash,
+                firstName: firstName,
+                lastName: lastName,
                 createdAt: new Date(),
                 updatedAt: new Date(),
-                role: 'USER', 
-                }}) ;
-        const token = jwt.sign({id: newUser.id}, process.env.JWT_SECRET);
-        // sendEmail(email, 
-        //     'Confirm your email', 
-        //     'Please click on the following link to confirm your email: http://localhost:3000/confirm?token=' 
-        //     + token)
-        res.json({token}) ;
+                role: 'USER',
+            }
+        });
+        const token = jwt.sign({ id: newUser.id, email: email }, process.env.JWT_SECRET);
+
+        let mail = sendEmail(email,
+            'Confirmez votre adresse email',
+            `Salut ${firstName.charAt(0).toUpperCase() + firstName.slice(1)}, <br> Pour activer ton compte clique sur le lien : <br> <a href="http://localhost:3000/confirm?token=${token}">http://localhost:3000/confirm?token=${token}</a>`);
+
+        res.json({ token });
     }
 });
 
 router.get('/verify', async (req, res) => {
-    const token = req.headers.authorization.split(' ')[1] ;
+    const token = req.headers.authorization.split(' ')[1];
     try {
-        const {id} = jwt.verify(token, process.env.JWT_SECRET) ;
-        const user = await prisma.user.findUnique({where: {id}}) ;
+        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await prisma.user.findUnique({ where: { id, confirmed: true } });
         if (user) {
-            res.json(user) ;
+            res.json(user);
         } else {
-            res.status(401).json({error: 'Invalid token'}) ;
+            res.status(401).json({ error: 'Invalid token' });
         }
     } catch (error) {
-        res.status(401).json({error: 'Invalid token'}) ;
+        res.status(401).json({ error: 'Invalid token' });
     }
 });
 
 router.patch('/confirm', async (req, res) => {
-    const token = req.headers.authorization.split(' ')[1] ;
+    const { token } = req.query;
     try {
-        const {id} = jwt.verify(token, process.env.JWT_SECRET) ;
-        const user = await prisma.user.update({where: {id}, data: {confirmed: true}}) ;
+        const { id, email } = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await prisma.user.update({ where: { id, email }, data: { confirmed: true } });
         if (user) {
             res.json(user);
             // sendEmail(user.email, 'Welcome to the app', 'Thank you for confirming your email') ;
         } else {
-            res.status(401).json({error: 'Invalid token'}) ;
+            res.status(401).json({ error: 'Invalid token' });
         }
     } catch (error) {
-        res.status(401).json({error: 'Invalid token'}) ;
+        res.status(401).json({ error: 'Invalid token' });
     }
 });
 
 router.patch('/reset-password', async (req, res) => {
-    const token = req.headers.authorization.split(' ')[1] ;
-    const {password} = req.body ;
+    const token = req.headers.authorization.split(' ')[1];
+    const { password } = req.body;
     try {
-        const {id} = jwt.verify(token, process.env.JWT_SECRET) ;
-        const hash = await bcrypt.hash(password, 10) ;
-        const user = await prisma.user.update({where: {id}, data: {password: hash}}) ;
+        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+        const hash = await bcrypt.hash(password, 10);
+        const user = await prisma.user.update({ where: { id }, data: { password: hash } });
         if (user) {
-            res.json(user) ;
+            res.json(user);
         } else {
-            res.status(401).json({error: 'Invalid token'}) ;
+            res.status(401).json({ error: 'Invalid token' });
         }
     } catch (error) {
-        res.status(401).json({error: 'Invalid token'}) ;
+        res.status(401).json({ error: 'Invalid token' });
     }
 });
 
 router.post('/forgot-password', async (req, res) => {
-    const {email} = req.body ;
-    const user = await prisma.user.findUnique({where: {email}}) ;
+    const { email } = req.body;
+    const user = await prisma.user.findUnique({ where: { email } });
     if (user) {
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET) ;
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
         // sendEmail(email,
         //     'Reset your password',
         //     'Please click on the following link to reset your password: http://localhost:3000/reset-password?token='
         //     + token) ;
-        res.json({token}) ;
+        res.json({ token });
     } else {
-        res.status(401).json({error: 'Invalid email'}) ;
+        res.status(401).json({ error: 'Invalid email' });
     }
 });
 
@@ -117,4 +118,4 @@ router.post('/forgot-password', async (req, res) => {
 
 
 
-module.exports = router ;
+module.exports = router;
--- back-end/services/nodemailer.js
+++ back-end/services/nodemailer.js
@@ -1,110 +1,102 @@
-// const nodemailer = require('nodemailer');
-// const { google } = require('googleapis');
-// const OAuth2 = google.auth.OAuth2;
+const nodemailer = require('nodemailer');
 
-// const oauth2Client = new OAuth2(
-//     process.env.CLIENT_ID, // ClientID
-//     process.env.CLIENT_SECRET, // Client Secret
-//     "https://developers.google.com/oauthplayground" // Redirect URL
-// );
+const transport = nodemailer.createTransport({
+    host: "ssl0.ovh.net",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
 
-// oauth2Client.setCredentials({
-//     refresh_token: process.env.REFRESH_TOKEN
-// });
-// const accessToken = oauth2Client.getAccessToken()
+const sendEmail = (to, subject, html) => {
+    let mailOptions = {
+        from: process.env.EMAIL,
+        to: to,
+        subject: subject,
+        html: html
+    };
 
-// const smtpTransport = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//         type: "OAuth2",
-//         user: process.env.EMAIL,
-//         clientId: process.env.CLIENT_ID,
-//         clientSecret: process.env.CLIENT_SECRET,
-//         refreshToken: process.env.REFRESH_TOKEN,
-//         accessToken: accessToken
+    console.log({
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+    })
+
+    // if (type === 'verify') {
+    //     mailOptions = {
+    //         ...mailOptions,
+    //         subject: "Email Confirmation",
+    //         html: `
+    //             <h2>Please click on given link to activate your account</h2>
+    //             <a href=${url}>${txt}</a>
+
+    //         `
+    //     };
+    // } else if (type === 'reset') {
+    //     mailOptions = {
+    //         ...mailOptions,
+    //         subject: "Password Reset",
+    //         html: `
+    //             <h2>Please click on given link to reset your password</h2>
+    //             <a href=${url}>${txt}</a>
+    //         `
+    //     };
+    // } else if (type === 'welcome') {
+    //     mailOptions = {
+    //         ...mailOptions,
+    //         subject: "Welcome to the team",
+    //         html: `
+    //             <h2>Welcome to the team</h2>
+    //             <p>Thank you for joining us. We are excited to have you on board.</p>
+    //         `
+    //     };
+    // } else if (type === 'goodbye') {
+    //     mailOptions = {
+    //         ...mailOptions,
+    //         subject: "We are sorry to see you go",
+    //         html: `
+    //             <h2>We are sorry to see you go</h2>
+    //             <p>Thank you for being a part of our team. We hope to see you again soon.</p>
+    //         `
+    //     };
+    // 
+    // }
+
+    transport.sendMail(mailOptions).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
+    })
+}
+
+
+
+module.exports = sendEmail;
+
+// Path: back-end/routes/auth.js
+// Compare this snippet from back-end/routes/auth.js:
+//     const token = req.headers.authorization.split(' ')[1] ;
+//     try {
+//         const {id} = jwt.verify(token, process.env.JWT_SECRET) ;
+//         const user = await prisma.user.findUnique({where: {id}}) ;
+//         if (user) {
+//             res.json(user) ;
+//         } else {
+
+//             res.status(401).json({error: 'Invalid token'}) ;
+//         }
+//     } catch (error) {
+//         res.status(401).json({error: 'Invalid token'}) ;
 //     }
 // });
-
-// const sendEmail = (to, url, txt, type) => {
-//     if (type === 'verify') {
-//         const mailOptions = {
-//             from: process.env.EMAIL,
-//             to: to,
-//             subject: "Email Confirmation",
-//             html: `
-//                 <h2>Please click on given link to activate your account</h2>
-//                 <a href=${url}>${txt}</a>
-
-//             `
-//         };
-//     } else if (type === 'reset') {
-//         const mailOptions = {
-//             from: process.env.EMAIL,
-//             to: to,
-//             subject: "Password Reset",
-//             html: `
-//                 <h2>Please click on given link to reset your password</h2>
-//                 <a href=${url}>${txt}</a>
-//             `
-//         };
-//     }  else if (type === 'welcome') {
-//         const mailOptions = {
-//             from: process.env.EMAIL,
-//             to: to,
-//             subject: "Welcome to the team",
-//             html: `
-//                 <h2>Welcome to the team</h2>
-//                 <p>Thank you for joining us. We are excited to have you on board.</p>
-//             `
-//         };
-//     } else if (type === 'goodbye') {
-//         const mailOptions = {
-//             from: process.env.EMAIL,
-//             to: to,
-//             subject: "We are sorry to see you go",
-//             html: `
-//                 <h2>We are sorry to see you go</h2>
-//                 <p>Thank you for being a part of our team. We hope to see you again soon.</p>
-//             `
-//         };
-
-//     }
-
-
-
-    
-
-
-//     smtpTransport.sendMail(mailOptions, (err, info) => {
-//         if (err) return err;
-//         return info;
-//     });
-// }   
-
-
-
-// module.exports = sendEmail;
-
-// // Path: back-end/routes/auth.js
-// // Compare this snippet from back-end/routes/auth.js:
-// //     const token = req.headers.authorization.split(' ')[1] ;
-// //     try {
-// //         const {id} = jwt.verify(token, process.env.JWT_SECRET) ;
-// //         const user = await prisma.user.findUnique({where: {id}}) ;
-// //         if (user) {
-// //             res.json(user) ;
-// //         } else {
-
-// //             res.status(401).json({error: 'Invalid token'}) ;
-// //         }
-// //     } catch (error) {
-// //         res.status(401).json({error: 'Invalid token'}) ;
-// //     }
-// // });
-// //
-// // router.patch('/confirm', async (req, res) => {
-// //     const token = req.headers.authorization.split(' ')[1] ;
-// //     try {        
+//
+// router.patch('/confirm', async (req, res) => {
+//     const token = req.headers.authorization.split(' ')[1] ;
+//     try {        
 
 
 
