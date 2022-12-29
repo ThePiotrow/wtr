115495c11f497e21bdcc45b2b30a8ab68e6cf07f
Florian GUILLOT <dread94@gmail.com>
2022-12-29 20:37:24 +0100

    fix routing and fix prisma

--- back-end/index.js
+++ back-end/index.js
@@ -7,6 +7,7 @@ const io = new Server(server);
 const { PrismaClient } = require('@prisma/client')
 const auth = require('./routes/auth');
 const rooms = require('./routes/rooms');
+const users = require('./routes/users');
 
 const prisma = new PrismaClient()
 
@@ -23,6 +24,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/room2', (req, res) => {
+  res.sendFile(__dirname + '/room2.html');
+});
+
+
 io.on('connection', (socket) => {
   console.log(`user connected ${socket.id}`);
   socket.on('disconnect', () => {
@@ -31,13 +37,20 @@ io.on('connection', (socket) => {
   socket.on('join_room', (room) => {
     console.log("data join room :", room);
     socket.join(room);
+    console.log(socket.rooms);
     // socket.to(room).emit('user_joined', user);
   });
   socket.on('message', ({message, room}) => {
     socket.to(room).emit('message', 
     {message, client: socket.id});
-    console.log('message: ' + message);
+    console.log(`message: ${message} room: ${room}`);
     io.emit('message', message);
+  });
+  socket.on('message2', ({message, room}) => {
+    socket.to(room).emit('message2', 
+    {message, client: socket.id});
+    console.log(`message2: ${message} room: ${room}`);
+    io.emit('message2', message);
   });
   
   // socket.on('send_message', (data) => {
@@ -67,6 +80,7 @@ io.on('connection', (socket) => {
 
 app.use('/auth', auth);
 app.use('/rooms', rooms);
+app.use('/users', users);
 // app.use('/users', require('./routes/users'));
 // app.use('/posts', require('./routes/posts'));
 // app.use('/profile', require('./routes/profile'));
