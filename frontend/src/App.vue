ec5afb6418a6b2c967a7342680b132f92e02237e
Matteo Delandhuy <matteodelandhuypro@outlook.fr>
2022-12-29 18:55:21 +0100

    frontend init

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+useHead({
+  title: 'Default Title',
+  meta: [
+    { name: 'description', content: 'Default Content' },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: computed(() => preferredDark.value ? '/icons/favicon-dark.svg' : '/icons/favicon.svg'),
+    },
+  ],
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    HELLO WORLD
+  </div>
+</template>
